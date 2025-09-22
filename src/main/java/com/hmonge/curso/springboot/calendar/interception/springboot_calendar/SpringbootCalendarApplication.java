618cf954c5716787e6012b5c8d4562694de821ae
Hernando Monge <hemogem@gmail.com>
2025-09-22 10:43:29 -0600

    first commit

---
+++ src/main/java/com/hmonge/curso/springboot/calendar/interception/springboot_calendar/SpringbootCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.hmonge.curso.springboot.calendar.interception.springboot_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmonge/curso/springboot/calendar/interception/springboot_calendar/SpringbootCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmonge.curso.springboot.calendar.interception.springboot_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
